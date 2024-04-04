866b98e769fb099a8f9267d64349d94a0349a99d
Donghyeon Kim <kdh0429@snu.ac.kr>
2024-04-04 14:50:57 +0900

    Seperate actor and critic optimizer

--- rsl_rl/runners/on_policy_runner.py
+++ rsl_rl/runners/on_policy_runner.py
@@ -37,6 +37,7 @@ from torch.utils.tensorboard import SummaryWriter
 import torch
 
 from rsl_rl.algorithms import PPO
+from rsl_rl.algorithms.ppo_tocabi import PPOTocabi
 from rsl_rl.modules import ActorCritic, ActorCriticRecurrent, ActorCriticTocabi
 from rsl_rl.env import VecEnv
 
